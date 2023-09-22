f907d949d3e4953ab939c0bd3cbee22401546098
Charles7c <charles7c@126.com>
2023-09-22 22:21:01 +0800

    style: 优化仪表盘二维码展示

--- continew-admin-ui/src/views/dashboard/workplace/components/carousel.vue
+++ continew-admin-ui/src/views/dashboard/workplace/components/carousel.vue
@@ -3,11 +3,13 @@
     indicator-type="slider"
     show-arrow="hover"
     auto-play
-    style="width: 100%; height: 300px; border-radius: 4px; overflow: hidden"
+    style="width: 100%; height: 282px; border-radius: 4px; overflow: hidden"
   >
     <a-carousel-item v-for="(src, idx) in imageSrc" :key="idx">
-      <div>
+      <div style="padding: 10px 30px 15px">
+        <div style="margin-bottom: 5px; text-align: center">相互 (huá shuǐ) 交流 (mō yú)</div>
         <img :src="src" style="width: 100%" alt="QrCode" />
+        <div style="text-align: center">扫码加VX，备注 cnadmin 进群</div>
       </div>
     </a-carousel-item>
   </a-carousel>
