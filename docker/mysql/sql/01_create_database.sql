5cb7af7856e87fe1cbf788398633fae97272d033
likanug <likanug.g@qq.com>
2025-06-13 15:13:06 +0800

    更新docker-compose-dev.yml，增加mysql的docker镜像启动初始化执行sql(创建continew_admin和continew_admin_job数据库)的配置

---
+++ docker/mysql/sql/01_create_database.sql
@@ -0,0 +1,10 @@
+-- ----------------------------
+-- 创建continew_admin数据库
+-- ----------------------------
+drop database if exists `continew_admin`;
+create database `continew_admin` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- ----------------------------
+-- 创建continew_admin_job数据库
+-- ----------------------------
+drop database if exists `continew_admin_job`;
+create database `continew_admin_job` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
